c0d700ca0e285e2823d9a8c9bd57ed1188961f38
Lukas Macko <llukas3@gmail.com>
2014-05-22 09:59:36 +0200

    4.5.x zmeny

--- datecharts/src/main/java/org/example/datecharts/RichBean.java
+++ datecharts/src/main/java/org/example/datecharts/RichBean.java
@@ -7,9 +7,14 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
-import org.richfaces.ui.output.chart.ChartDataModel.ChartType;
-import org.richfaces.ui.output.chart.DateChartDataModel;
-import org.richfaces.ui.output.chart.NumberChartDataModel;
+import org.richfaces.model.ChartDataModel.ChartType;
+import org.richfaces.model.NumberChartDataModel;
+import org.richfaces.model.PlotClickEvent;
+import org.richfaces.model.StringChartDataModel;
+
+//import org.richfaces.ui.output.chart.ChartDataModel.ChartType;
+//import org.richfaces.ui.output.chart.DateChartDataModel;
+//import org.richfaces.ui.output.chart.NumberChartDataModel;
 
 @ViewScoped
 @ManagedBean
@@ -19,23 +24,50 @@ public class RichBean implements Serializable {
 
     private String name;
     private NumberChartDataModel model;
-    private DateChartDataModel dateModel;
+    private NumberChartDataModel numberBar;
+    private StringChartDataModel pie;
+    private StringChartDataModel bar;
+    private StringChartDataModel abar;
+    String msg;
+   // private DateChartDataModel dateModel;
 
 
     @PostConstruct
     public void postContruct() {
+    	msg="nothing";
         name = "John";
         model = new NumberChartDataModel(ChartType.line);
         model.put(1, 3);
         model.put(2, 3);
         model.put(3, 4);
         model.put(4, 5);
-
+        
+        numberBar = new NumberChartDataModel(ChartType.bar);
+        numberBar.put(1, 3);
+        numberBar.put(2, 3);
+        numberBar.put(3, 4);
+        numberBar.put(4, 5);
+        
+        pie = new StringChartDataModel(ChartType.pie);
+        pie.put("A", 4);
+        pie.put("B", 5);
+        pie.put("C", 6);
+        
+        abar = new StringChartDataModel(ChartType.bar);
+        abar.put("A", 4);
+        abar.put("B", 5);
+        abar.put("C", 6);
+        
+        bar = new StringChartDataModel(ChartType.bar);
+        bar.put("A", 4);
+        bar.put("B", 5);
+        bar.put("C", 6);
+/*
         dateModel = new DateChartDataModel(ChartType.line);
         dateModel.put(new GregorianCalendar(2013, 4, 1).getTime(), 4);
         dateModel.put(new GregorianCalendar(2013, 4, 2).getTime(), 5);
         dateModel.put(new GregorianCalendar(2013, 4, 3).getTime(), 6);
-
+*/
 
     }
 
@@ -50,8 +82,28 @@ public class RichBean implements Serializable {
     public NumberChartDataModel getModel() {
         return model;
     }
+    
+    public StringChartDataModel getPie() {
+		return pie;
+	}
 
-    public DateChartDataModel getDateModel() {
-        return dateModel;
+    public StringChartDataModel getBar() {
+		return bar;
+	}
+    public StringChartDataModel getAbar() {
+		return abar;
+	}
+    
+    public NumberChartDataModel getNumberBar() {
+		return numberBar;
+	}
+//    public DateChartDataModel getDateModel() {
+//        return dateModel;
+//    }
+    public void handler(PlotClickEvent ev){
+        msg="I'm alive";	
     }
+    public String getMsg() {
+		return msg;
+	}
 }
